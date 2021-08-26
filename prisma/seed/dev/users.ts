ef6f7a3f08c0c34ea47b32752f9ab73f4805dc02
Albert Luta <luta.albert100@gmail.com>
2021-08-26 15:55:16 +0300

    feat(seed-dev): user's last name should be all caps

--- prisma/seed/dev/users.ts
+++ prisma/seed/dev/users.ts
@@ -6,7 +6,7 @@ export const users: SeedDev<Prisma.UserCreateManyInput>[] = [
 		email: 'test@test.com',
 		fatherInitial: 'N',
 		firstName: 'Ion',
-		lastName: 'Popescu',
+		lastName: 'POPESCU',
 		password:
 			'$argon2i$v=19$m=4096,t=3,p=1$ie/5oMvgcybXIM9UlD1zdg$f747QByyfh/Ui2Z8UftV0Uu+KPvtl2aPvDfkXPtSFq4',
 		avatar: '_development/users/test/avatar.webp'
@@ -15,7 +15,7 @@ export const users: SeedDev<Prisma.UserCreateManyInput>[] = [
 		email: 'test2@test.com',
 		fatherInitial: 'I',
 		firstName: 'Andrei',
-		lastName: 'Ionescu',
+		lastName: 'IONESCU',
 		password:
 			'$argon2i$v=19$m=4096,t=3,p=1$ie/5oMvgcybXIM9UlD1zdg$f747QByyfh/Ui2Z8UftV0Uu+KPvtl2aPvDfkXPtSFq4',
 		avatar: '_development/users/test2/avatar.webp'
@@ -24,7 +24,7 @@ export const users: SeedDev<Prisma.UserCreateManyInput>[] = [
 		email: 'test3@test.com',
 		fatherInitial: 'A',
 		firstName: 'Maria',
-		lastName: 'Popa',
+		lastName: 'POPA',
 		password:
 			'$argon2i$v=19$m=4096,t=3,p=1$ie/5oMvgcybXIM9UlD1zdg$f747QByyfh/Ui2Z8UftV0Uu+KPvtl2aPvDfkXPtSFq4',
 		avatar: '_development/users/test3/avatar.webp'
@@ -33,7 +33,7 @@ export const users: SeedDev<Prisma.UserCreateManyInput>[] = [
 		email: 'test4@test.com',
 		fatherInitial: 'S',
 		firstName: 'Alexandra',
-		lastName: 'Dumitrescu',
+		lastName: 'DUMITRESCU',
 		password:
 			'$argon2i$v=19$m=4096,t=3,p=1$ie/5oMvgcybXIM9UlD1zdg$f747QByyfh/Ui2Z8UftV0Uu+KPvtl2aPvDfkXPtSFq4',
 		avatar: '_development/users/test4/avatar.webp'
@@ -42,7 +42,7 @@ export const users: SeedDev<Prisma.UserCreateManyInput>[] = [
 		email: 'test5@test.com',
 		fatherInitial: 'T',
 		firstName: 'Stefan',
-		lastName: 'Tudor',
+		lastName: 'TUDOR',
 		password:
 			'$argon2i$v=19$m=4096,t=3,p=1$ie/5oMvgcybXIM9UlD1zdg$f747QByyfh/Ui2Z8UftV0Uu+KPvtl2aPvDfkXPtSFq4',
 		avatar: '_development/users/test5/avatar.webp'
